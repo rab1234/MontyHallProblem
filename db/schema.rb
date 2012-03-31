983210a475a425dfd2704e05eac3c46338facd43
rab1234 <rab1234@uslex.net>
2012-03-31 10:22:19 -0500

    added SessionID column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120331042521) do
+ActiveRecord::Schema.define(:version => 20120331151919) do
 
   create_table "games", :force => true do |t|
     t.integer  "w_door"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120331042521) do
     t.integer  "other_door"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "SessionID"
   end
 
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -3,15 +3,13 @@ require 'spec_helper'
 describe GamesController do
   describe 'store session id' do
     it 'should store session id' do
-      
+      pending
     end
   end
   
   describe 'select random door' do
     it 'should return a random number between 1 and 3' do
-      #n = get_random_door
-      #n.should(be > 0) && n.should(be < 4)
-      #Game.should_receive(:get_random_door)
+      pending
     end
   end
 end

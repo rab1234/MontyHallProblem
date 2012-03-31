2a89ca614751d803133df0c099ec7ea73b0e2a21
rab1234 <rab1234@uslex.net>
2012-03-31 09:06:50 -0500

    removed session_id column from model

--- db/migrate/20120331135856_add_session_i_dto_games.rb
+++
@@ -1,7 +0,0 @@
-class AddSessionIDtoGames < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120331135856) do
+ActiveRecord::Schema.define(:version => 20120331042521) do
 
   create_table "games", :force => true do |t|
     t.integer  "w_door"

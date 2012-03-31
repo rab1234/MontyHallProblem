1df8aa3baf5f6df59e6ec587ec3dea14511bf4ea
rab1234 <rab1234@uslex.net>
2012-03-31 00:19:29 -0500

    created some rspec specs to test model CRUD actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,13 +4,25 @@ class GamesController < ApplicationController
   end
   
   def play
+    # user has clicked "Yes!"
     @step = 2
     @game = Game.create!(:w_door => rand(3) + 1)
     render 'index'
   end
   
   def choose
+    # user has selected a door
     @step = 3
+    @game = Game.first  #need to find by user session
+    @game.update_attribute(:u_door, params[:door])
     render 'index'
+  end
+  
+  # -- private --
+  private
+  def pick_show_door
+    g = Game.first
+    # remove u_door and w_door from array [1,2,3]
+    # remaining door(s) is/are available to show
   end
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Game" do
+  describe "CRUD actions" do
+    before(:each) do
+      @game = Game.create!(:w_door => 1, :u_door => 2, :show_door => 3, :other_door => 3)
+    end
+    it "saves an instance in the database" do
+      Game.count.should be > 0
+    end
+    
+    it "updates attributes" do
+      @game.update_attribute(:u_door, 1)
+      g = Game.first
+      g.u_door.should == 1
+    end
+  end
+end
